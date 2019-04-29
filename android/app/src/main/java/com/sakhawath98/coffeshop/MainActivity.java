29a10e662f159307ddf19c2345459237be599af2
Sakhawat238 <sakhawath98@gmail.com>
2019-04-29 17:29:41 +0600

    android: security update

--- android/app/src/main/java/com/sakhawath98/coffeshop/MainActivity.java
+++ android/app/src/main/java/com/sakhawath98/coffeshop/MainActivity.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+
+//package for security feature
+import android.view.WindowManager.LayoutParams;
+
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    //code for preventing taking screenshot & showing a dark ui instead
+    //of app ui in recent items list.
+    getWindow().addFlags(LayoutParams.FLAG_SECURE);
   }
 }
