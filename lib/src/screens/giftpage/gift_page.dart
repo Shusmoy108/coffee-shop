741f21693834a0729e2769107a7cdb0d23d47083
Sakhawat238 <sakhawath98@gmail.com>
2019-05-01 13:08:39 +0600

    QRscanner added

--- lib/src/screens/giftpage/gift_page.dart
+++ lib/src/screens/giftpage/gift_page.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'loyalty_card_ui.dart';
 import 'terms_and_conditions.dart';
+import 'package:qrcode_reader/qrcode_reader.dart';
 
 class GiftPage extends StatefulWidget {
   @override
@@ -8,7 +10,8 @@ class GiftPage extends StatefulWidget {
 }
 
 class _GiftPageState extends State<GiftPage> {
-  int score = 12;
+  int score = 0;
+  String barcode = '';
 
   showClaimConfirmDialog() {
     if (score == 12) {
@@ -50,6 +53,35 @@ class _GiftPageState extends State<GiftPage> {
     }
   }
 
+  showSlotFullDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Can not add score'),
+          content: SingleChildScrollView(
+            child: Text(
+              'Please claim your free drink first.',
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                'Dismiss',
+                style: TextStyle(
+                  color: Colors.redAccent,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,13 +106,27 @@ class _GiftPageState extends State<GiftPage> {
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: score == 12 ? null : () {},
+        onPressed: score == 12 ? showSlotFullDialog : scan,
         label: Text(
           'Add Score',
         ),
-        icon: Icon(Icons.add_circle_outline),
+        icon: Icon(Icons.camera_alt),
         backgroundColor: Color.fromRGBO(220, 20, 60, 0.8),
       ),
     );
+  }
+
+  Future scan() async {
+    try {
+      String barcode = await QRCodeReader().scan();
+      setState(() {
+        this.barcode = "Result: $barcode";
+        this.score++;
+      });
+    } catch (e) {
+      setState(() {
+        this.barcode = 'Error: $e';
+      });
+    }
   }
 }
